3495f43ca58a3a31f7d90a2e272a5f44d34e0c79
KC Sivaramakrishnan <sk826@cl.cam.ac.uk>
2015-03-05 01:21:07 +0000

    Functorizing last-writer-wins register.

--- examples/lww_test.ml
+++ examples/lww_test.ml
@@ -10,11 +10,11 @@ let key = [ "local"; "register1" ]
 let read t = Irmin.read (t "Reading the register") key
 
 let write t v =
-  let reg_val = Lww_register.new_value v in
+  let reg_val = Lww_register.Int.new_value v in
   Irmin.update (t "Updating the register") key reg_val
 
 let main () =
-  let store = Irmin.basic (module Irmin_git.FS) (module Lww_register) in
+  let store = Irmin.basic (module Irmin_git.FS) (module Lww_register.Int) in
   let config = Irmin_git.config ~root:"/tmp/irmin/test" ~bare:true()  in
   Irmin.create store config task >>= fun t ->
     write t 10 >>= fun () ->
--- lib/lww_register.ml
+++ lib/lww_register.ml
@@ -1,43 +1,54 @@
 (* Last-write-wins register *)
 
-open Lwt
-open Irmin_unix
-open Core
+module Lww_core (Value : Tc.S0) = struct
 
-let () =
-  try match Sys.getenv "DEBUG" with
-    | "" -> ()
-    | _ ->
-      Log.color_on ();
-      Log.set_log_level Log.DEBUG
-  with Not_found -> ()
+  open Lwt
+  open Irmin_unix
+  open Core
 
-module type TIME = module type of Time
+  let () =
+    try match Sys.getenv "DEBUG" with
+      | "" -> ()
+      | _ ->
+        Log.color_on ();
+        Log.set_log_level Log.DEBUG
+    with Not_found -> ()
 
-module Time = struct
-  include Tc.Bin_prot0 (struct
-    include Time
-    let to_json v = Ezjsonm.float @@ to_float v
-    let of_json v = of_float @@ Ezjsonm.get_float v
-    let bin_size_t v = Bin_prot.Size.bin_size_float @@ to_float v
-    let bin_write_t a ~pos c = Bin_prot.Write.bin_write_float a ~pos (to_float c)
-    let bin_read_t a ~pos_ref = Bin_prot.Read.bin_read_float a ~pos_ref |> of_float
-  end)
-  include (Time : TIME with type t := t)
+  module type TIME = module type of Time
+
+  module Time = struct
+    include Tc.Bin_prot0 (struct
+      include Time
+      let to_json v = Ezjsonm.float @@ to_float v
+      let of_json v = of_float @@ Ezjsonm.get_float v
+      let bin_size_t v = Bin_prot.Size.bin_size_float @@ to_float v
+      let bin_write_t a ~pos c = Bin_prot.Write.bin_write_float a ~pos (to_float c)
+      let bin_read_t a ~pos_ref = Bin_prot.Read.bin_read_float a ~pos_ref |> of_float
+    end)
+    include (Time : TIME with type t := t)
+  end
+
+  module Path = Irmin.Path.String_list
+  include Tc.Pair (Time)(Tc.Int)
+
+  let compare (x,u) (y,v) =
+    let r = Pervasives.compare x y in
+    if r == 0 then Pervasives.compare u v
+    else r
+
+  let new_value value = (Time.now (), value)
+
+  let merge _path ~old t1 t2 =
+    let open Irmin.Merge.OP in
+    if compare t1 t2 < 0 then ok t2 else ok t1
+
+  let merge path = Irmin.Merge.option (module Tc.Pair(Time)(Tc.Int)) (merge path)
+
 end
 
-module Path = Irmin.Path.String_list
-include Tc.Pair (Time)(Tc.Int)
-
-let compare (x,u) (y,v) =
-  let r = Pervasives.compare x y in
-  if r == 0 then Pervasives.compare u v
-  else r
-
-let new_value value = (Time.now (), value)
-
-let merge _path ~old t1 t2 =
-  let open Irmin.Merge.OP in
-  if compare t1 t2 < 0 then ok t2 else ok t1
-
-let merge path = Irmin.Merge.option (module Tc.Pair(Time)(Tc.Int)) (merge path)
+module Int = Lww_core (Tc.Int)
+module String = Lww_core (Tc.String)
+module Int32 = Lww_core (Tc.Int32)
+module Int64 = Lww_core (Tc.Int64)
+module Unit = Lww_core (Tc.Unit)
+module Bool = Lww_core (Tc.Bool)
